b9c1b0298e87e422dabd017d04f26b2afac6f2a2
rickypeng99 <rickypeng99@gmail.com>
2019-03-06 15:43:52 -0600

    first commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Router, Route, Switch} from 'react-router-dom'
+import {Router, Route, Switch} from 'react-router'
 import 'semantic-ui-css/semantic.min.css'
 
 
